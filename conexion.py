97205930007286f5d68dff695b630001511f2e89
Usuario <Usuario>
2017-06-19 10:45:03 -0500

    se creo la clase invernadero

--- conexion.py
+++ conexion.py
@@ -1,18 +1,19 @@
 import sqlite3
 
-db = sqlite3.connect("invernadero.db")
-c = db.cursor()
+class Invernadero:
+    db = sqlite3.connect("invernadero.db")
+    c = db.cursor()
 
-def mostrar_usuario():
-    r = c.execute("SELECT *  FROM usuario")
-    r = r.fetchall()
-    
-    for fila in r:
-        print(fila)
-        
-def insertar_usuario(datos):
-    c.execute("INSERT INTO usuario (nombre, apellido1, apellido2, correo, passwoord, tipo) VALUES(?,?,?,?,?,?)", (datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]))
-    db.commit()
-    
-insertar_usuario(["m", "d", "b", "m@gmail.com", "1", 1])
-mostrar_usuario()
+    def mostrar_usuario(self):
+        r = self.c.execute("SELECT *  FROM usuario")
+        r = r.fetchall()
+        lista = []
+
+        for fila in r:
+            lista.append(fila)
+            
+        return lista
+
+    def insertar_usuario(self, datos):
+        self.c.execute("INSERT INTO usuario (nombre, apellido1, apellido2, correo, passwoord, tipo) VALUES(?,?,?,?,?,?)", (datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]))
+        self.db.commit()

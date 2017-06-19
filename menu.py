722454a4e93ee72fdc57e55fd9eef74b50faab1a
Usuario <Usuario>
2017-06-19 11:31:42 -0500

    creacion del metodo capturar para insertar datos

--- menu.py
+++ menu.py
@@ -11,7 +11,7 @@ class Menu:
             if op == "1":
                 self.mostrar()
             elif op == "2":
-                pass
+                self.capturar()
             elif op == "0":
                 break
                 
@@ -19,4 +19,14 @@ class Menu:
         usuarios = self.inv.mostrar_usuario()
         for u in usuarios:
             print("{0:2} {1:10} {2:8} {3:8} {4:15} {5:6} {6:1}".format(u[0], u[1], u[2], u[3], u[4], u[5], u[6]))
+            
+    def capturar(self):
+        nombre = input("Nombre: ")
+        apellido1 = input("Apellido 1: ")
+        apellido2 = input("Apellido 2: ")
+        correo = input("Correo: ")
+        password = input("Password: ")
+        tipo = input("Tipo: ")
+        
+        self.inv.insertar_usuario([nombre, apellido1, apellido2, correo, password, tipo])
                   

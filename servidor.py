621564197b274635a789f6d58b165b58b05fd7b7
Usuario <Usuario>
2017-06-22 10:51:16 -0500

    regresa los usuarios en JSON

--- servidor.py
+++ servidor.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from conexion import Invernadero
+
 app = Flask("bd")
+inv = Invernadero()
 
 @app.route("/")
 def raiz():
     return "Hola"
+
+@app.route("/usuarios")
+def usuarios():
+    usuarios = inv.mostrar_usuario()
+    print(usuarios)
+    respuesta = jsonify(usuarios)
+    return respuesta
 
 app.run()

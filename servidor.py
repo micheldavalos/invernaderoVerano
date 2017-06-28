b8d7e0707c944ad3dcc302e22aa45e5a23c6fabf
Usuario <Usuario>
2017-06-28 11:39:04 -0500

    se creo la funcion que toma los datos del formulario y los guarda en la base de datos

--- servidor.py
+++ servidor.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from conexion import Invernadero
 
 app = Flask("bd")
@@ -13,6 +13,22 @@ def usuarios():
     usuarios = inv.mostrar_usuario()
     print(usuarios)
     respuesta = jsonify(usuarios)
+    respuesta.headers.add('Access-Control-Allow-Origin', '*')
+    return respuesta
+
+@app.route("/agregar_usuario", methods=['POST'])
+def agregar_usuario():
+    print(request.form)
+    nombre = request.form['nombre']
+    apellido1 = request.form['apellido1']
+    apellido2 = request.form['apellido2']
+    correo = request.form['correo']
+    password = request.form['password']
+    tipo = request.form['tipo']
+    
+    inv.insertar_usuario([nombre, apellido1, apellido2, correo, password, tipo])
+    
+    respuesta = jsonify({'status': 'Ok'})
     respuesta.headers.add('Access-Control-Allow-Origin', '*')
     return respuesta
 
